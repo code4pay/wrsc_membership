22dff9b6a98ec0faadc247879b042ca64f01ffb7
Michael Mueller <michael@muellers.net.au>
2020-05-02 16:08:55 +1000

    Revert to Backpack40 start adding permissions

---
+++ resources/views/vendor/backpack/crud/fields/sibling_members.blade.php
@@ -0,0 +1,112 @@
+<!-- field_type_name -->
+<div class="row col-lg-12" >
+<table class="table .table-responsive table-striped ">
+<thead>
+<tr><th>Member Number</th><th>Name</th><th>Member type</th><tr>
+@if ( isset($entry) && $entry->siblings()->count() > 0)
+@foreach ($entry->siblings()->get()  as  $attributes)
+    <tr> <td>{{ $attributes->member_number }} </td><td><a href="/admin/user/{{ $attributes['id'] }}/show"> {{ $attributes['fullname'] }}</a> </td><td>{{ $attributes->memberType->name }}</td><td>
+</td> </tr>
+
+
+     @endforeach
+@endif
+</table>
+</div>
+@push('after_scripts') @if ($crud->getRequest()->ajax()) @endpush @endif
+<script>
+
+	if (typeof deleteEntry != 'function') {
+	  $("[data-button-type=delete]").unbind('click');
+
+	  function deleteEntry(button) {
+		// ask for confirmation before deleting an item
+		// e.preventDefault();
+		var button = $(button);
+		var route = button.attr('data-route');
+		var row = $("#crudTable a[data-route='"+route+"']").closest('tr');
+
+		swal({
+		  title: "{!! trans('backpack::base.warning') !!}",
+		  text: "{!! trans('backpack::crud.delete_confirm') !!}",
+		  icon: "warning",
+		  buttons: {
+		  	cancel: {
+			  text: "{!! trans('backpack::crud.cancel') !!}",
+			  value: null,
+			  visible: true,
+			  className: "bg-secondary",
+			  closeModal: true,
+			},
+		  	delete: {
+			  text: "{!! trans('backpack::crud.delete') !!}",
+			  value: true,
+			  visible: true,
+			  className: "bg-danger",
+			}
+		  },
+		}).then((value) => {
+			if (value) {
+				$.ajax({
+			      url: route,
+			      type: 'DELETE',
+			      success: function(result) {
+			          if (result == 1) {
+			          	  // Show a success notification bubble
+			              new Noty({
+		                    type: "success",
+		                    text: "{!! '<strong>'.trans('backpack::crud.delete_confirmation_title').'</strong><br>'.trans('backpack::crud.delete_confirmation_message') !!}"
+		                  }).show();
+
+			              // Hide the modal, if any
+			              $('.modal').modal('hide');
+                          location.reload();
+
+			              // Remove the row from the datatable
+			              row.remove();
+			          } else {
+			              // if the result is an array, it means 
+			              // we have notification bubbles to show
+			          	  if (result instanceof Object) {
+			          	  	// trigger one or more bubble notifications 
+			          	  	Object.entries(result).forEach(function(entry, index) {
+			          	  	  var type = entry[0];
+			          	  	  entry[1].forEach(function(message, i) {
+					          	  new Noty({
+				                    type: type,
+				                    text: message
+				                  }).show();
+			          	  	  });
+			          	  	});
+			          	  } else {// Show an error alert
+				              swal({
+				              	title: "{!! trans('backpack::crud.delete_confirmation_not_title') !!}",
+	                            text: "{!! trans('backpack::crud.delete_confirmation_not_message') !!}",
+				              	icon: "error",
+				              	timer: 4000,
+				              	buttons: false,
+				              });
+			          	  }			          	  
+			          }
+			      },
+			      error: function(result) {
+			          // Show an alert with the result
+			          swal({
+		              	title: "{!! trans('backpack::crud.delete_confirmation_not_title') !!}",
+                        text: "{!! trans('backpack::crud.delete_confirmation_not_message') !!}",
+		              	icon: "error",
+		              	timer: 4000,
+		              	buttons: false,
+		              });
+			      }
+			  });
+			}
+		});
+
+      }
+	}
+
+	// make it so that the function above is run after each DataTable draw event
+	// crud.addFunctionToDataTablesDrawEventQueue('deleteEntry');
+</script>
+@if (!$crud->getRequest()->ajax()) @endpush @endif
