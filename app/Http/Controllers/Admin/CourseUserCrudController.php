31edd27c197a80c6295a623e2d19d506ac317e4d
vagrant <vagrant@homestead>
2020-05-03 00:05:08 +0000

    remove document upload on course completed for now

--- app/Http/Controllers/Admin/CourseUserCrudController.php
+++ app/Http/Controllers/Admin/CourseUserCrudController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\CourseUserRequest;
 use Backpack\CRUD\app\Http\Controllers\CrudController;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
 use App\Http\Requests\CourseUserRequest as StoreRequest;
+
 /**
  * Class CourseUserCrudController
  * @package App\Http\Controllers\Admin
@@ -37,7 +38,7 @@ class CourseUserCrudController extends CrudController
     {
 
         $this->crud->setTitle('Add a Course to User');
-       $userId =  \Request::input('user_id'); // default value
+        $userId =  \Request::input('user_id'); // default value
         $this->addDefinedFields($userId);
         $this->crud->setValidation(StoreRequest::class);
         // TODO: remove setFromDb() and manually define Fields
@@ -45,48 +46,50 @@ class CourseUserCrudController extends CrudController
 
     protected function setupUpdateOperation()
     {
-       $userId =  \Request::has('user_id')?\Request::has('user_id'):false; // default value
+        $userId =  \Request::has('user_id') ? \Request::has('user_id') : false; // default value
         $this->addDefinedFields($userId);
         $this->crud->setValidation(StoreRequest::class);
     }
 
-    protected function addDefinedFields ($userId) {
+    protected function addDefinedFields($userId)
+    {
 
         $this->crud->addFields([
-           [ 
-            'label' => "User",
-            'type' => 'text',
-            'name' => 'user_id', // the db column for the foreign key
-            'default' => $userId,
-            'attributes' => [
-                'class' => '',
-                'readonly'=>'readonly'
+            [
+                'label' => "User",
+                'type' => 'text',
+                'name' => 'user_id', // the db column for the foreign key
+                'default' => $userId,
+                'attributes' => [
+                    'class' => '',
+                    'readonly' => 'readonly'
+                ],
+
             ],
+            [
+                'label' => "Course",
+                'type' => 'select',
+                'name' => 'course_id', // the db column for the foreign key
+                'entity' => 'course', // the method that defines the relationship in your Model
+                'attribute' => 'name',
+            ],
+            [
+                'label' => "Course By",
+                'type' => 'text',
+                'name' => 'course_by', // the db column for the foreign key
+            ],
+            [
+                'label' => "Date Completed",
+                'type' => 'date',
+                'name' => 'date_completed', // the db column for the foreign key
+            ],
+            // [   // Upload
+            //     'name'   => 'upload',
+            //     'label'  => 'Upload',
+            //     'type'   => 'upload',
+            //     'upload' => true,
 
-           ],
-           [ 
-            'label' => "Course",
-            'type' => 'select',
-            'name' => 'course_id', // the db column for the foreign key
-            'entity' => 'course', // the method that defines the relationship in your Model
-            'attribute' => 'name', 
-           ],
-           [ 
-            'label' => "Course By",
-            'type' => 'text',
-            'name' => 'course_by', // the db column for the foreign key
-           ],
-           [ 
-            'label' => "Date Completed",
-            'type' => 'date',
-            'name' => 'date_completed', // the db column for the foreign key
-           ], [   // Upload
-            'name'   => 'upload',
-            'label'  => 'Upload',
-            'type'   => 'upload',
-            'upload' => true,
-
-        ]
-       ]);
+            // ]
+        ]);
     }
 }
