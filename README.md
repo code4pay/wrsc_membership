b7cd3190bb3aa80aeea99367b43b1f1a75900e3f
Michael Mueller <michael@muellers.net.au>
2021-01-24 09:47:48 +1100

    Initial Commit

--- README.md
+++ README.md
@@ -1,28 +1,13 @@
 # Membership Database Focused on Wildlife rescue
 
+## Memberhip Renewals
+Before every membership renewal period you will need to update the `CURRENT_PAID_TO`
+date in the `.env` file.  Generally set to the end of June the following year in 
+`2022-06-30` format. 
 
-This project is based on Laravel and Backpack. 
-## About Laravel
-
-Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel takes the pain out of development by easing common tasks used in many web projects, such as:
-
-- [Simple, fast routing engine](https://laravel.com/docs/routing).
-- [Powerful dependency injection container](https://laravel.com/docs/container).
-- Multiple back-ends for [session](https://laravel.com/docs/session) and [cache](https://laravel.com/docs/cache) storage.
-- Expressive, intuitive [database ORM](https://laravel.com/docs/eloquent).
-- Database agnostis [schema migrations](https://laravel.com/docs/migrations).
-- [Robust background job processing](https://laravel.com/docs/queues).
-- [Real-time event broadcasting](https://laravel.com/docs/broadcasting).
-
-Laravel is accessible, powerful, and provides tools required for large, robust applications.
-
-## Learning Laravel
-
-Laravel has the most extensive and thorough [documentation](https://laravel.com/docs) and video tutorial library of all modern web application frameworks, making it a breeze to get started with the framework.
-
-If you don't feel like reading, [Laracasts](https://laracasts.com) can help. Laracasts contains over 1500 video tutorials on a range of topics including Laravel, modern PHP, unit testing, and JavaScript. Boost your skills by digging into our comprehensive video library.
-
+## Fees 
+Fees are currently hard coded 
 
 ## Development environment
-You can geta working development environment using Homestead and vagrant.  
+See Notes.md
 
--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\BackpackUser;
 
 class ApplicationController extends Controller
 {
@@ -34,7 +35,34 @@ class ApplicationController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request);
+
+        $password = str_random(50);
+        $request->merge(['password' => $password]);
+        $request->merge(['password_confirmation' => $password]);
+        $latest_membership_id = BackpackUser::max('member_number');
+        if (!$request->input('member_number')) {
+            $request->merge(['member_number' => $latest_membership_id + 1]);
+        }
+        $user = BackpackUser::create($request->all());
+        $user->save();
+        return view('application_uploads',['user' => $user]); 
+    }
+  /**
+     * Add a Mebership Card image
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function id_upload(Request $request)
+    {
+        
+        $user = BackpackUser::findOrFail($request->input('user_id'));
+        if (!$user) {
+            abort(400, 'Could not find that entry in the database X.');
+        }
+        $user->image = $request->input('image');
+        $user->save();
+
     }
 
 
@@ -49,37 +77,4 @@ class ApplicationController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
