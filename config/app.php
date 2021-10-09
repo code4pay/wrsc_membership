084c5c31331e8d42fd43cbde7527b4d40cf80255
Michael Mueller <michael@muellers.net.au>
2021-10-10 07:59:45 +1100

    Add missing Image class after laravel Upgrade.

--- config/app.php
+++ config/app.php
@@ -207,7 +207,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+         Intervention\Image\ImageServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -275,7 +275,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
     'Debugbar' => Barryvdh\Debugbar\Facade::class,
-
+        'Image' => Intervention\Image\Facades\Image::class,
     ],
 
 ];
