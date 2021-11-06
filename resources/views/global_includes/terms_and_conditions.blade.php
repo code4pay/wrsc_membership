52e1faab9909ba8ec0d89e2f75a4990407e790af
Michael Mueller <michael@muellers.net.au>
2021-11-06 16:27:36 +1100

    Fix issue where the bullet points where doubling up

--- resources/views/global_includes/terms_and_conditions.blade.php
+++ resources/views/global_includes/terms_and_conditions.blade.php
@@ -1,21 +1,21 @@
 
 <ol>
-    <li>1.      I agree to be bound by the conditions of : </li>
+    <li>I agree to be bound by the conditions of : </li>
     <ol type="a">
-      <li>a: Licence number MWL000100253 issued to Wildlife Rescue South Inc by NPWS. </li>
-      <li>b: <a href="/uploads/wildlife_rescue_constitution_2011.pdf"> Constitution </a>, rules and guidelines of Wildlife Rescue South Coast Inc.  </li>
-      <li>d:   Fund Raising Authority Number CFN16681 issued by the Office of Liquor, Gaming and Racing.
-      <li>c: Policies and Procedures of Wildlife Rescue South Coast Inc </li>
+      <li>Licence number MWL000100253 issued to Wildlife Rescue South Inc by NPWS. </li>
+      <li><a href="/uploads/wildlife_rescue_constitution_2011.pdf"> Constitution </a>, rules and guidelines of Wildlife Rescue South Coast Inc.  </li>
+      <li>Fund Raising Authority Number CFN16681 issued by the Office of Liquor, Gaming and Racing.
+      <li>Policies and Procedures of Wildlife Rescue South Coast Inc </li>
     </ol>
-    <li>2.      I agree to be guided by the WRSC Coordinators and WRSC Committee in every facet of 
+    <li>I agree to be guided by the WRSC Coordinators and WRSC Committee in every facet of 
       wildlife care and rehabilitation and will allow the relevant coordinators or Committee members to
       inspect my premises at a time of mutual agreement. </li>
-    <li>3.      I agree to return any property loaned to me by WRSC when requested by the Committee. </li>
-    <li>4.      I agree that all my labour is voluntary and that all activities are carried out at my own risk
+    <li>I agree to return any property loaned to me by WRSC when requested by the Committee. </li>
+    <li>I agree that all my labour is voluntary and that all activities are carried out at my own risk
       and with no right of recourse against any member of WRSC. </li>
-    <li>5.      I declare that I am not a prohibited person under the Commission for Children and Young People
+    <li>I declare that I am not a prohibited person under the Commission for Children and Young People
       Act 2007. </li>
-    <li>6.      I declare that I have not been convicted on charges relating to wildlife / domestic animals.
+    <li>I declare that I have not been convicted on charges relating to wildlife / domestic animals.
     </li>
   </ol>
 <p>Should any of these conditions be breached by any member, membership can be terminated by the committee and become effective immediately by letter in writing to the applicable member.</p>
