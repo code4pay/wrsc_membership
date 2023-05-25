bf1ea9e3a012174a8cb17772989d882b77dd55dc
Michael Mueller <michael@muellers.net.au>
2023-05-25 14:48:07 +0800

    Update email_renewal_request.blade.php

--- resources/views/membership_renewal/email_renewal_request.blade.php
+++ resources/views/membership_renewal/email_renewal_request.blade.php
@@ -3,27 +3,71 @@
 ### Member Number: {{$user->member_number }}  Name: {{$user->name}}    
 
  Dear {{ $user->first_name }}
-We sincerely thank you for being part of WRSC.  Although it is hard to believe, it is now membership renewal time!
+<p>
+The Wildlife Rescue South Coast committee thanks you for your support and being part of
+our community of wildlife carers. Although it is hard to believe, another year has passed and
+that it’s renewal time again. We ask that you take a moment to renew as soon as possible –
+so it doesn’t get forgotten!
+</p>
+<p>
+We are also looking to simplify our membership arrangements next year, so we urge you to
+read the attached President’s report to learn about this changing environment, and what it
+means for you - our members. This report will also explain what has happened in the last 12
+months.
+</p>
+<p>
+<b>
+Last year we discounted the renewal cost by 50% to $15 for Primary members as
+recognition of the fact that COVID prevented us from delivering some services to our
+members. Although the 2022 renewal was to have been the last time we discounted our
+fees, we have decided to extend the discount to this year’s renewals. We know times are
+tough so hopefully this small token helps a little.
+<b>
+</p>
+<p>
+After years of flood, fire and COVID, we are finally emerging into an environment which is in
+some ways different to the years prior to COVID. Part of this is due to the increased
+government regulation of the wildlife rehabilitation sector, which impacts on the way we go
+about our wildlife rescue and rehabilitation operations and has a huge impact on how
+training is delivered. NPWS has created new valuable Codes of Practice for many species of
+wildlife, and some of our coordinators have contributed to these Codes.
+</p>
+<p>
+We welcome members who joined us this year and gratefully acknowledge the hard work
+carried out by our volunteers, whether involved in rescues and rehabilitation, fundraising,
+administration, publicity, communications, or any of the other vital roles needed to keep
+this organisation running. We hope those who haven’t, will attend an introduction to
+wildlife rescue in the very near future.
+</p>
+<p>
+In this email you will find a link to your membership record, where you can check and
+update personal details. When your details are correct, all you need to do is acknowledge
+the terms and conditions and remit the funds as shown on the acknowledgement screen. If
+you do not want to renew online, please email membership@wildlife-rescue.org.au and I
+will post your renewal form.
+</p>
 
-We would like to acknowledge the wide variety of hard work that is carried out by our members in the rescue, care and release of our wildlife, as well as keeping the organisation ticking over in the background.  It has been a difficult couple of years for many of our members with fire, flood and COVID. It has also been a difficult time for wildlife. To see what has happened during the past 12 months at WRSC, please see the attached President’s report.
-
-Welcome to all our new members who have joined us this past year. 
-
-To support the efforts of our carers, for our new members who have not been able to fully partake in activities, and because of the generosity of donors, we are continuing to discount the renewal cost by 50% to $15 for Primary members for this year only. 
-
-In this email you will find a link to your membership record, where you can check and update personal details. When your details are correct, all you need to do is acknowledge the terms and conditions and remit the funds as shown on the acknowledgement screen. If you do not want to renew online, please email me and I will post your renewal form. 
- 
-Wherever possible, we will be emailing your acknowledgement letters along with a pdf of your membership card, which you may print out and upload to your smartphone. If you would like a laminated membership card, please let me know.
-
+<p>
+Wherever possible, we will be emailing your acknowledgement letters along with a pdf of
+your membership card, which you may print out and upload to your smartphone. If you
+would like a laminated membership card, please let me know.
+</p>
+<p>
  <a href="{{url('/tac_accept')}}/{{$user->createToken('tac')}}"> Click this link to renew your membership online </a>. For family 
  memberships, each family member must renew by clicking on the link. <i>Only the
  person identified as the primary family member will be asked to pay</i>
-
+</p>
+<p>
 If you don't wish to renew please <a href="{{url('/dont_renew')}}/{{$user->createToken('tac')}}"> Click here to let us know you won't be renewing </a> 
-
-If you have animals in care, and have not paid membership by the commencement of the new financial year i.e. 1st July 2022 then you will not be covered by WRSC’s NPWS licence MWL000100253 or our insurance and your authorisation to care, if applicable, will no longer be current.
-
-Thank you again for your membership, thank you for your support and look forward to receiving your renewal as soon as possible.
+</p>
+<p>
+<b>It is important that all renewals are confirmed by 1 July 2023</b>. If you have not paid
+membership by then, you will not be covered by WRSC’s NPWS licence to rehabilitate, or by
+insurance. If you currently have authority care for wildlife – that also expires at the same
+time as your membership.
+Thank for your continued support and I look forward to receiving your renewal as soon as
+possible.
+</p>
 
 
  Yours Sincerely <br/>   
